f007931b763f29e45cc515847b86c91f8e5d67de
Pieter Svenson <pietelite@gmail.com>
2022-04-08 22:47:10 -0500

    more linting

--- common/src/test/java/me/pietelite/nope/common/struct/AltSetTest.java
+++ common/src/test/java/me/pietelite/nope/common/struct/AltSetTest.java
@@ -28,7 +28,6 @@ import java.util.AbstractCollection;
 import java.util.Arrays;
 import java.util.HashSet;
 import java.util.stream.IntStream;
-
 import me.pietelite.nope.common.api.struct.AltSet;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
